f6fa2c476f7c9197fd737959ca80c99811c09508
Fabian2964 <fabianpaul1@gmx.de>
2022-07-09 10:56:09 +0200

    add

--- app.py
+++ app.py
@@ -342,7 +342,7 @@ if st.button('Submit'):
    
     # Get prediction
     prediction = np.exp(rf.predict(df_final))
-    predicition = round(prediction, 0)
+    prediction = round(prediction, 0)
     
     # Output prediction
     st.text(f'This Article will generate {prediction} Visits.')
